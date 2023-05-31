9dca7d0b6a20e6eaf58053296b525fd22e3087ed
adityar22 <adityaramadhanarckarito47@gmail.com>
2023-05-31 19:38:11 +0700

    add searchbar

--- frontend-1/src/pages/Admin/StudentList.jsx
+++ frontend-1/src/pages/Admin/StudentList.jsx
@@ -1,6 +1,12 @@
+import Searchbar from "../../components/public/searchbar";
+
+
 const StudentList = () => {
     return ( 
         <>
+        <div className="justify-center items-center py-20 lg:py-10 px-3 lg:px-28 h-screen" >
+            <Searchbar/>
+            </div>
         </>
      );
 }
